3b15952e248ed13f56ebb6e2620d0a4fabc41d64
parveen99 <40237237+parveen99@users.noreply.github.com>
2021-04-18 02:06:50 +0530

    Create integersspace.js
    Write a code to get the input in the given format and print the output in the given format
    
    Input Description:
    A single line contains integers separated by space
    
    Output Description:
    Print the integer list of integers separated by space
    
    Sample Input :
    2 3 4 5 6 7 8
    
    Sample Output :
    2 3 4 5 6 7 8

---
+++ integersspace.js
@@ -0,0 +1,25 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+let str = '';
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic
+  for (i=0;i<userInput.length;i++){
+      str += userInput[i] + ''
+  }
+
+  console.log(str);
+
+  //end-here
+});
